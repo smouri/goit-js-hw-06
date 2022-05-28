fb10c4a64df01f5d8317db86c7463914c9e6c9e3
Roman Babiy <romchik.babiy@gmail.com>
2022-05-28 14:46:54 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
+const ingredientsCont = document.querySelector("#ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const createList = (el) => {
+    let li = document.createElement("li");
+    li.textContent = el;
+    li.classList.add("item");
+    return li;
+};
+const list = ingredients.map(createList);
+ingredientsCont.append(...list);
