6ab74867987dac9b2030c5dc84226c75539c3044
Roman Babiy <romchik.babiy@gmail.com>
2022-06-17 13:55:13 +0300

    add inputSize

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputSize = document.querySelector("#font-size-control");
 const textSize = document.querySelector("#text");
-
+textSize.style.fontSize = `${inputSize.value}px`;
 inputSize.addEventListener("input", (event) => {
     textSize.style.fontSize = `${event.target.value}px`;
 });
